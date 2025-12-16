362670185bf58c6601a7e19805010382dd1c7cd4
zaynabaurangzaibkhan167-ctrl <zaynabaurangzaibkhan167@gmail.com>
2025-12-16 13:26:55 +0500

    Add files via upload

---
+++ TASK 4/Task 4.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+cout   <<"Welcome to c++ Lab!" << endl;
+cout   <<"You will learn basic output today. "<< endl;
+cout   <<"Let's start with simple messages." ;
+system("pause") ;
+return 0;
+}
