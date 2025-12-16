2a2ee98a81ff655c06de1f13c57b1aefc9e7a24f
zaynabaurangzaibkhan167-ctrl <zaynabaurangzaibkhan167@gmail.com>
2025-12-16 13:24:15 +0500

    Add files via upload

---
+++ TASK 2/Task 2.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+cout   <<"she said, \" Let's learn C++! \"\nTo start, save your file in c : \\c++_Projects\\Lab1\nGood luck! ";
+system("pause") ;
+return 0;
+}
