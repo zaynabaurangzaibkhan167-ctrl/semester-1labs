472f65b56d2375f0a9cfb9a70f12ee04a292bbdd
zaynabaurangzaibkhan167-ctrl <zaynabaurangzaibkhan167@gmail.com>
2025-12-16 13:20:26 +0500

    Delete task 1 directory

--- task 1/zaynabb1.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-int main()
-{
-	int x;
-	cout<<"enter the integer  ";
-	cin >> x;
-	(x % 2 == 0)? cout <<"the number  " << x <<" is even": cout << "the number" << x <<" is odd";  
-}
