3bbae18ae1774978b4fffade8e00c2f93735a352
zaynabaurangzaibkhan167-ctrl <zaynabaurangzaibkhan167@gmail.com>
2025-12-16 13:28:57 +0500

    Add files via upload

---
+++ TASK 1/Task 1.cpp
@@ -0,0 +1,22 @@
+ #include<iostream>
+using namespace std;
+int main ()
+{
+//declaring and intialilizing variables
+	int a;
+	int b;
+	a = 5;
+	b =10;
+//opreating basic arthimitc operations	
+	int Add = a+b;
+	int sub = a-b;
+	int mul = a*b;
+	int div = a/b;
+//displaying the values of arthimitic values	
+	cout <<"The addition of "<<a<<" and "<<b<< "is:"<<Add<<endl;
+	cout <<"The subtraction of "<<a<<" and "<<b<< "is:"<<sub<<endl;
+	cout <<"The multiplication of "<<a<<" and "<<b<< "is:"<<mul<<endl;;
+	cout <<"The division of "<<a<<" and "<<b<< "is:"<<div<<endl;
+	return 0;
+
+}
