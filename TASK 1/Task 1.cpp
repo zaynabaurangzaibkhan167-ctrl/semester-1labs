022c3f2bf940ddb7aecd65d67ce54501b0794e45
zaynabaurangzaibkhan167-ctrl <zaynabaurangzaibkhan167@gmail.com>
2025-12-16 13:57:16 +0500

    Add files via upload

---
+++ TASK 1/Task 1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+//declaring variables	
+	int n;
+	int i;
+//taking upper limit form user
+	cout<<"Enter the upper limit"<<endl;
+	cin>>n;
+//using for loop	
+	for(i=1; i<=n; i++)
+	{
+		cout<<i<<endl;	
+	}
+}
